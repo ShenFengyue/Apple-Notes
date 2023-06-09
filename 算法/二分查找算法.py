a2c126bad3dde8354ff955d48ec4e328599ba964
ShenFengyue <122566131+ShenFengyue@users.noreply.github.com>
2023-06-10 01:54:43 +0800

    Create 二分查找算法.py

---
+++ 算法/二分查找算法.py
@@ -0,0 +1,25 @@
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1  # 目标元素不存在
+
+# 示例用法
+arr = [1, 3, 5, 7, 9]
+target = 5
+result = binary_search(arr, target)
+if result != -1:
+    print("目标元素在索引", result, "处")
+else:
+    print("目标元素不存在")
+
